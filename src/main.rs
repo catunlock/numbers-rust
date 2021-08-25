547dcf13856fc6263aa11306504e1dfe23ef0dd2
Alberto López Sánchez <alopez@cells.es>
2021-08-25 13:29:09 +0200

    first step, read numbers from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,35 @@
-use core::num;
-use std::collections::HashMap;
+use std::io::{self, BufRead, Read};
+use std::collections::HashSet;
 
-#[derive(Debug)]
-enum Map<'a> {
-    Value(&'a HashMap<u8, Map<'a>>),
-    Nil,
-}
-
-enum Mapa {
-    Value(Vec<Mapa>),
-    Nil
-}
-
-
-impl<'a> Map<'a> {
-    fn print(self) {
-        if let Value(h) = self {
-            for (number , map) in h {
-                print!("{} ->", number);
-                map.print();
-            }
-            
-        }
-    }
-
-    fn check(self, v: &str) -> bool {
-        if let Value(h) = self {
-            let c = v.chars().next().expect("Error the string is empty");
-            let number: u8 = c.to_digit(10).expect("Error converting char to int") as u8;
-
-            print!("{}", number);
-
-            match h.get(&number) {
-                Some(next) => {
-                    let t = &v[1..];
-                    next.check(t);
-                }
-                None => ()
-            }
-        }
-        else if let Nil = self {
-            ()
-        }
-        false
-    }
-}
-
-
-use Map::{Value, Nil};
-
-fn main() {
+fn main() -> io::Result<()> {
     println!("Backend Challenge 2021!");
 
-    let empty = HashMap::new();
-    let list = Value(&empty);
+    let mut numbers = HashSet::new();
+
+    println!("Enter the allowed numbers (-1 to exit):");
     
-    if let Value(h) = list {
-        let mut h3 = HashMap::new();
-        h3.insert(4, Nil);
-
-        h.insert(2, Value(&h3));
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        match line {
+            Err(_) => break,    // with ^Z
+            Ok(s) => {
+                let i : i64 = s.parse().expect("Error converting input to index position.");
+                println!("Echo {}", i);
+                if i >= 0 {
+                    numbers.insert(i);
+                }
+                else {
+                    break;
+                }
+            }
+        }
     }
-
+    
+    for n in numbers {
+        print!("{}, ", n);
+    }
 
     println!("end");
 
+    Ok(())
 }
