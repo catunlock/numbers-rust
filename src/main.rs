9ec358d68d55c688525b70843a2ba71636009a6f
Alberto López Sánchez <alopez@cells.es>
2021-08-25 17:39:51 +0200

    convert to tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,47 @@
-use std::io::{self, BufRead, Read};
+use std::io::{self, BufRead, BufReader};
 use std::collections::HashSet;
+use std::net::{Shutdown, TcpListener, TcpStream};
+
+
+fn handle_connection(stream: TcpStream, numbers: &mut HashSet<u64>) -> Result<(), std::io::Error> {
+    let mut reader = BufReader::new(&stream);
+
+    let mut buf = String::with_capacity(10);
+    match reader.read_line(&mut buf) {
+        Ok(size_r) => {
+            println!("Recv: {:?}, size: {}", buf, size_r);
+
+            match buf.parse::<u64>() {
+                Ok(i) => { numbers.insert(i); }
+                Err(e) => {println!("Error converting string to int {}", e)}
+            }
+            
+        }
+        Err(e) => {
+            println!("Error reading from the buffer: {}", e);
+        }
+    }
+
+    stream.shutdown(Shutdown::Both)
+}
 
 fn main() -> io::Result<()> {
     println!("Backend Challenge 2021!");
 
-    let mut numbers = HashSet::new();
+    let mut numbers: HashSet<u64> = HashSet::new();
+    let address = "127.0.0.1:8000";
 
     println!("Enter the allowed numbers (-1 to exit):");
     
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        match line {
-            Err(_) => break,    // with ^Z
-            Ok(s) => {
-                let i : i64 = s.parse().expect("Error converting input to index position.");
-                println!("Echo {}", i);
-                if i >= 0 {
-                    numbers.insert(i);
-                }
-                else {
-                    break;
-                }
-            }
+    let listener = TcpListener::bind("127.0.0.1:8000")
+        .expect(format!("Error creating TCP socket at {}", address).as_str());
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established!");
+        match handle_connection(stream, &mut numbers) {
+            Ok(()) => {}
+            Err(e) => {println!("Error handling clinet: {}", e)}
         }
     }
     
